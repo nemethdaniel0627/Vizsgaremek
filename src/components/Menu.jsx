48f83fee8f938825d52021c80cf81dae25aa7425
nemethdaniel0627 <nem.dani0627@gmail.com>
2021-10-03 12:11:16 +0200

    Sötét téma implementálása Norbi html-jéből, étlap lapozás folytatása

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,13 +6,12 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
 export default function Menu(props) {
     function currentDayColorize() {
 
-        const day = new Date().getDay();
+        const day = new Date().getDay();        
         let currentDay;
-        if (day === 6 || day === 7) {
+        if (day === 6 || day === 0) {
             currentDay = document.getElementById("day-" + 1);
         }
-        else currentDay = document.getElementById("day-" + day);
-
+        else currentDay = document.getElementById("day-" + day);        
         if (currentDay) {
             currentDay.classList.toggle("menu--day-selected");
         }
@@ -20,11 +19,37 @@ export default function Menu(props) {
 
     function setCurrentWeek() {
         const year = new Date().getFullYear();
-        const month = new Date().getMonth();       
+        const month = new Date().getMonth();
         const startDay = new Date(year, month, ((new Date().getDate()) - (new Date().getDay() - 1)));
         const endDay = new Date(year, month, ((new Date().getDate()) - (new Date().getDay() - 1)) + 4)
         let finalString = `${year}.${(startDay.getMonth() + 1) < 10 ? "0" : ""}${(startDay.getMonth() + 1)}.${startDay.getDate() < 10 ? "0" : ""}${startDay.getDate()} - ${(endDay.getMonth() + 1) < 10 ? "0" : ""}${(endDay.getMonth() + 1)}.${endDay.getDate() < 10 ? "0" : ""}${endDay.getDate()}`;
+        const firstArrow = document.getElementById("weekArrow-1");
+        if (firstArrow) firstArrow.classList.add("hidden");
         return finalString;
+    }
+
+    function weekChange(event) {
+        let arrow = event.target.attributes[8];
+        if (arrow === undefined) {
+            arrow = event.target.attributes[0].ownerElement.parentNode.id;
+        }
+        else {
+            arrow = arrow.value;
+        }
+        const firstArrow = document.getElementById("weekArrow-1");
+        switch (arrow) {
+            case "weekArrow-1":            
+                if (firstArrow) firstArrow.classList.add("hidden");
+                break;
+
+            case "weekArrow-2":                
+                if (firstArrow) firstArrow.classList.remove("hidden");
+                break;
+
+            default:
+                break;
+        }
+
     }
 
     useEffect(() => {
@@ -36,13 +61,13 @@ export default function Menu(props) {
         <div className="menu">
             <h2 className="menu--header">
                 Étlap
-                <span className="menu--week-header">
-                    <span id="weekArrow-1" className="menu--week-header--icon"><FontAwesomeIcon icon={faChevronLeft}/></span>
-                    {setCurrentWeek()}
-                    <span id="weekArrow-2" className="menu--week-header--icon"><FontAwesomeIcon icon={faChevronRight}/></span>
+                <span className="menu--week-header">                    
+                    <FontAwesomeIcon className="menu--week-header--icon hidden" onClick={weekChange} id="weekArrow-1" icon={faChevronLeft} />
+                    {setCurrentWeek()}                    
+                    <FontAwesomeIcon className="menu--week-header--icon" onClick={weekChange} id="weekArrow-2" icon={faChevronRight} />
                 </span>
             </h2>
-            
+
 
             <div className="menu--wrapper">
                 <MenuDays id="day-1" dayName="Hétfő" appetizer="Meggyes almaleves" mainCourse="BBQ-s sült csirkecomb Rizs Káposztasaláta" />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
-import React from "react";
-import foodE_logo from "../images/FoodWeb_logo.png"
+import React, { useState } from "react";
+import foodE_logo from "../images/FoodWeb_logo.png";
+import lightModeImg from "../images/ToDefault.png";
+import darkModeImg from "../images/ToDark.png";
 
 export default function Navbar() {
+    const [darkMode, setDarkMode] = useState(false);
+
+    function darkModeChange() {
+        let tmpDarkMode = !darkMode;
+        setDarkMode(!darkMode);
+        const root = document.querySelector(':root');
+        if (tmpDarkMode) {
+            console.log("asd");
+            root.style.setProperty('--dark-blue', "#0D1321");
+            root.style.setProperty('--light-blue', "#1D2D44");
+            root.style.setProperty('--blue', "#3E5C76");
+            root.style.setProperty('--bodyBackground', "#3E5C76");
+            root.style.setProperty('--darkModeFontColor', "#fff");
+        }
+        else {
+            root.style.setProperty('--dark-blue', "#001E6C");
+            root.style.setProperty('--blue', "#035397");
+            root.style.setProperty('--light-blue', "#5089C6");
+            root.style.setProperty('--bodyBackground', "#fff");
+            root.style.setProperty('--darkModeFontColor', "#000");
+        }
+    }
 
     // function LogingInApper() {
     //     let letterHead = document.getElementById("LetterHead");
@@ -87,7 +111,7 @@ export default function Navbar() {
     //         if (elem) {
     //             console.log("asd2");
     //             if (letterHead) {
-                    
+
     //                 if (body) {
     //                     console.log("asd3");
     //                     let id = null;
@@ -158,11 +182,18 @@ export default function Navbar() {
     // function LoginAndPageChanger() { }
 
     return (
-        <div id="header" className="header">            
+        <div id="header" className="header">
             <label htmlFor="header--btn" id="header--photo" className="header--photo">
                 {/* <span className="header--img" onclick="LogingInApper()"></span> */}
                 <img alt="ICON" id="Logo" src={foodE_logo} />
             </label>
+            <div className="darkmode">
+                <label className="switch">
+                    <input type="checkbox" onClick={darkModeChange} id="Dark_check" />
+                    <span className="slider round"></span>
+                </label>
+                <img src={darkMode ? darkModeImg : lightModeImg} alt="DarkMode" className="darkmode--img" />
+            </div>
         </div>
 
     );
