94164149b6b5b1c6b09bfbc4f51e6269a79507a4
rozsnono <rozsnono@gmail.com>
2021-10-22 19:45:19 +0200

    **Admin felújítás, javítás**

--- SZAKDOGA/JavaAdmin.js
+++ SZAKDOGA/JavaAdmin.js
@@ -1,18 +1,21 @@
 var CurrentlyIndex = 0;
+var table =  "<tr style='background-color: #2196f3BB'><th></th><th id = 'table_name'><a href = '#' onclick = 'OrderBy(name)'>Név </a></th><th id = 'table_class'><a href = '#' onclick = 'OrderBy(class)'>Osztály &#8205 &#8205 &#8205</a></th><th>Befizetve</th><th>Befizetett összeg</th><th>Lemondott napok</th></tr>";
+var data = "";
 function PageLoaded()
 {
-    var table =  "<table><tr style='background-color: #2196f3BB'><th></th><th id = 'table_name'><a href = '#' onclick = 'OrderBy(name)'>Név &#8681 </a></th><th id = 'table_class'><a href = '#' onclick = 'OrderBy(class)'>Osztály &#8679</a></th><th>Befizetve</th><th>Befizetett összeg</th><th>Lemondott napok</th></tr>";
-    var data = ["Habsburg Mária Terézia;12.A;true;15000;2021-09-27#2021-09-26#2021-09-25"];
-    var zindex = 9;
-    for (let index = 0; index < 10; index++) {
-        if (index % 30/(4) == 0) {
-            zindex++;
+    
+    this.data = ["Habsburg Mária Terézia;12.A;true;15000;2021-09-27#2021-09-26#2021-09-25"];
+    for (let index = 0; index < 20; index++) {
+        var zindex = Math.floor(Math.random() * 4) + 9;
+        if (Math.floor(Math.random() * 100) % 3) {
+            
+            this.data.push("Habsburg Mária Terézia;"+ zindex +".A;false;0;");  
         }
-        data.push("Habsburg Mária Terézia;"+ zindex +".A;true;15000;2021-09-27#2021-09-26#2021-09-25");    
+        this.data.push("Habsburg Mária Terézia;"+ zindex +".A;true;15000;2021-09-27#2021-09-26#2021-09-25");    
         
         
     }
-    document.querySelector("#DataBase_Table table").innerHTML += TableLoader(data);
+    document.querySelector("#DataBase_Table table").innerHTML += TableLoader(this.data);
 }
 
 function SelectRow(row)
@@ -29,13 +32,37 @@ function SelectRow(row)
 function Register()
 {
     document.getElementById("Register_btn").style.display = "none";
+    document.getElementsByClassName("button")[1].style.display = "block";
+
+    for (const button of document.getElementsByTagName("button")) {
+        button.disabled = true;
+    }
+
+    document.getElementById("NewOne_btn").disabled = false;
+    document.getElementById("Upload_btn").disabled = false;
+    document.getElementById("RegisterCancel_btn").disabled = false;
+}
+
+function Search()
+{
+    document.getElementById("Search_btn").style.display = "none";
     document.getElementsByClassName("button")[0].style.display = "block";
 }
 
-function Cancel()
+function Cancel(which)
 {
-    document.getElementById("Register_btn").style.display = "block";
-    document.getElementsByClassName("button")[0].style.display = "none";
+    for (const button of document.getElementsByTagName("button")) {
+        button.disabled = false;
+    }
+
+    if(which == 0){
+        document.getElementById("Search_btn").style.display = "block";
+    }else
+    {
+        document.getElementById("Register_btn").style.display = "block";
+    }
+    
+    document.getElementsByClassName("button")[which].style.display = "none";
     document.getElementsByClassName("custom-file-upload")[0].innerHTML = "<input type = 'file' id = 'file' onchange = UploadFile()><img src='./Images/File_icon.png' alt='Fájl' style='width: 20px;'> Fájl kiválasztása"
 }
 
@@ -60,13 +87,13 @@ function TextAbstract(text,length)
 function NewOne()
 {
     var datapertable= "";
-    datapertable += "<tr><td><input type = 'button' value = '+' style = ' height:32px; margin: 2px'></td>";
-    datapertable += "<td><input id = 'new_name' type = 'text' placeholder = 'Név' style = 'width: 250px; text-align:center; height:32px'></td>";
-    datapertable += "<td><input id = 'new_class' type = 'text' placeholder = 'Osztály' style = 'width: 90px; text-align:center; height:32px'></td>";
+    datapertable += "<tr><td></td>";
+    datapertable += "<td><input id = 'new_name' type = 'text' placeholder = 'Név'  style = 'width: 220px'></td>";
+    datapertable += "<td><input id = 'new_class' type = 'text' placeholder = 'Osztály'  style = 'width: 90px'></td>";
     datapertable += "<td> <input type = 'checkbox' id = 'cb_new'/></td>";
-    datapertable += "<td></td>";
-    datapertable += "<td></td></tr>";
-    document.querySelector("#DataBase_Table table").innerHTML += datapertable;
+    datapertable += "<td><input type = 'button' value = 'Mentés'  style = 'width: 150px'   class = 'btn btn-success' onclick = ''></td>";
+    datapertable += "<td><input type = 'button' value = 'Mégsem' style = 'width: 150px'  class = 'btn btn-danger' onclick = 'RegistrationCancel()'></td></tr>";
+    document.querySelector("#DataBase_Table table").innerHTML = table + datapertable + TableLoader(this.data);
 }
 
 function TableLoader(data)
@@ -78,21 +105,25 @@ function TableLoader(data)
         for (let dataIndex = 0; dataIndex < data[index].split(';').length; dataIndex++) {
             const element = data[index].split(';');
             if (element[dataIndex].toLowerCase() == "true") {
-                datapertable += "<td> <input type = 'checkbox' checked disabled/> </td>";
+                datapertable += "<td> <img class = 'PaidImage' src='./Images/Correct_icon.png' alt='Igen'> </td>";
             }else if(element[dataIndex].toLowerCase() == "false")
             {
-                datapertable += "<td> <input type = 'checkbox' disabled/> </td>";
+                datapertable += "<td> <img class = 'PaidImage' src='./Images/Incorrect_icon.png' alt='Nincs'> </td>";
             }else if(dataIndex == element.length-2)
             {
                 datapertable += "<td>" + element[dataIndex] + " Ft </td>" 
             }else if(dataIndex == element.length-1)
             {
-                datapertable += "<td> <select>"
-                for (const item of element[dataIndex].split('#')) {
-                    datapertable += "<option>" + item + "</option>" 
-                    
+                if(element[dataIndex] != "" && element[dataIndex] != null)
+                {
+                    datapertable += "<td> <select>"
+                    for (const item of element[dataIndex].split('#')) {
+                        datapertable += "<option>" + item + "</option>" 
+                        
+                    }
+                    datapertable += "</select></td>"
                 }
-                datapertable += "</select></td>"
+                
             }
             else{
                 datapertable += "<td> " + element[dataIndex] + "</td>";
@@ -110,26 +141,40 @@ function OrderBy(type)
 {
     switch (type) {
         case "name":
-            if (index[0] == 0) {
-                document.getElementById("table_name").innerHTML = "<a href = '#' onclick =OrderBy('name')>Név &#8681 </a>";
-                index[0] = 1;
-            }else
+            switch(index[0])
             {
-                document.getElementById("table_name").innerHTML = "<a href = '#' onclick = OrderBy('name')>Név &#8679 </a>";
-                index[0] = 0;
+                case 0:
+                    document.getElementById("table_name").innerHTML = "<a href = '#' onclick =OrderBy('name')>Név &#8681 </a>";
+                    index[0]++;
+                    break;
+                case 1: 
+                    document.getElementById("table_name").innerHTML = "<a href = '#' onclick = OrderBy('name')>Név &#8679 </a>";
+                    index[0]++;
+                    break;
+                case 2: 
+                    document.getElementById("table_name").innerHTML = "<a href = '#' onclick =OrderBy('name')>Név </a>";
+                    index[0] = 0;
+                    break;
             }
             index[1] = 0;
             document.getElementById("table_class").innerHTML = "<a href = '#' onclick = OrderBy('class')>Osztály &#8205 &#8205 &#8205</a>";
             break;
             
         case "class":
-            if (index[1] == 0) {
-                document.getElementById("table_class").innerHTML = "<a href = '#' onclick = OrderBy('class')>Osztály &#8681 </a>";
-                index[1] = 1;
-            }else
+            switch(index[1])
             {
+                case 0:
+                    document.getElementById("table_class").innerHTML = "<a href = '#' onclick = OrderBy('class')>Osztály &#8681 </a>";
+                    index[1]++;
+                    break;
+                case 1: 
                 document.getElementById("table_class").innerHTML = "<a href = '#' onclick = OrderBy('class')>Osztály &#8679 </a>";
-                index[1] = 0;
+                    index[1]++;
+                    break;
+                case 2: 
+                document.getElementById("table_class").innerHTML = "<a href = '#' onclick = OrderBy('class')>Osztály &#8205 &#8205 &#8205</a>";
+                    index[1] = 0;
+                    break;
             }
             index[0] = 0;
             document.getElementById("table_name").innerHTML = "<a href = '#' onclick = OrderBy('name')>Név </a>";
@@ -166,12 +211,17 @@ function Download(file="Táblázat")
             column6 = row.cells[5].innerText;
         }else
         {
-            for (let index = 0; index < row.cells[5].innerText.split('\n').length; index++) {
-                if ( row.cells[5].innerText.split('\n')[index] == null) {
-                    break;
+            try {
+                for (let index = 0; index < row.cells[5].innerText.split('\n').length; index++) {
+                    if ( row.cells[5].innerText.split('\n')[index] == null) {
+                        break;
+                    }
+                    column6 +=  row.cells[5].innerText.split('\n')[index] + ',';
                 }
-                column6 +=  row.cells[5].innerText.split('\n')[index] + ',';
+            } catch (error) {
+                
             }
+           
         }
         rows.push(
             [
@@ -198,4 +248,14 @@ function Download(file="Táblázat")
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
+}
+
+function RegistrationCancel()
+{
+    document.querySelector("#DataBase_Table table").deleteRow(1);
+}
+
+function Delete()
+{
+    //document.querySelector("#DataBase_Table table").deleteRow(1);   
 }
