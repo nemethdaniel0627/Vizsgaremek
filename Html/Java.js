da12b049c7c7b06dbcaf41f402afaa74a11104b8
rozsnono <rozsnono@gmail.com>
2021-11-24 16:49:27 +0100

    **Page oldal (véglegesítés)** Navbar fejlesztés ++ Fixes #23

--- Html/Java.js
+++ Html/Java.js
@@ -4,6 +4,8 @@ var Chosable = [];
 var CurrentPage = "Login";
 var Days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 var MenuPerWeek = [];
+var User = {};
+var QRcodeText = "";
 
 var errorIcon ='<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="mb-1 bi bi-exclamation-triangle" viewBox="0 0 16 16"><path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/><path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z"/></svg>';
 var infoIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mb-1 bi bi-info-circle" viewBox="0 0 16 16">'
@@ -32,6 +34,29 @@ function PageLoaded() {
       : sessionStorage.getItem("CurrentPage");
 
   sessionStorage.setItem('SavedUser', localStorage.getItem('ToPageUser') == null ? sessionStorage.getItem('SavedUser') : localStorage.getItem('ToPageUser'));
+  this.User = JSON.parse(sessionStorage.getItem("SavedUser"));
+  if(User.paid != "true")
+  {
+    for (const lis of document.getElementsByClassName("isPaid")) {
+      lis.style.display = "none";
+    }
+  }
+
+  nameUser = User.name.split('');
+
+  nameUser[nameUser.indexOf('á')] = 'a';
+  nameUser[nameUser.indexOf('é')] = 'e';
+  nameUser[nameUser.indexOf('í')] = 'i';
+  nameUser[nameUser.indexOf('ó')] = 'o';
+  nameUser[nameUser.indexOf('ö')] = 'o';
+  nameUser[nameUser.indexOf('ő')] = 'o';
+  nameUser[nameUser.indexOf('ú')] = 'u';
+  nameUser[nameUser.indexOf('ü')] = 'u';
+  nameUser[nameUser.indexOf('ű')] = 'u';
+
+
+  QRcodeText = nameUser.join('') + ";" + User.Userclass + ";" + User.schoolOM + ";" + User.paid + ";" + NowDate();
+  QRcodeLoader(QRcodeText);
 
   sessionStorage.setItem(
     "Logined",
@@ -94,6 +119,25 @@ function PageLoaded() {
     (date.getDate() < 10 ? "0" + date.getDate() : date.getDate());
   document.getElementById("FirstDay").value = dates;
   document.getElementById("LastDay").value = dates;
+}
+
+function QRcodeLoader(dataRow)
+{
+  let website = dataRow;
+  if (website) {
+    let qrcodeContainer = document.getElementById("qrcode");
+    qrcodeContainer.innerHTML = "";
+    new QRCode(qrcodeContainer, {
+      text: website,
+      width: 180,
+      height: 180,
+      colorDark: "#000000",
+      colorLight: "#ffffff00",
+      correctLevel: QRCode.CorrectLevel.H
+    });
+  } else {
+    console.log(error);
+  }
 }
 
 function DatesLoader() {
@@ -178,8 +222,6 @@ function CurrentDayColorize() {
   } else {
     document.getElementsByClassName(CurrentDayPicker())[0].style ="display: block!important; background-image: linear-gradient(to bottom, #6a8f6b, #1D2D44);";
   }
-  // document.getElementsByClassName(CurrentDayPicker())[0].className =
-  //   "col-lg-3 col-sm-12 Weekday " + CurrentDayPicker();
 }
 
 function CurrentDayPicker() {
@@ -188,17 +230,6 @@ function CurrentDayPicker() {
   ];
 }
 
-function NotUsedDayDelete() {
-  // for (const day of Days) {
-  //   if (CurrentDayPicker() != day) {
-  //     for (const dayclasses of document.getElementsByClassName(day)) {
-  //       dayclasses.style.display = "none";
-  //     }
-  //   }
-  // }
-
-  
-}
 
 sessionStorage.setItem("NextWeek", 0);
 
@@ -460,7 +491,7 @@ function NowDate() {
 
 function LogingIn() {
 
-  var User = JSON.parse(sessionStorage.getItem("SavedUser"));
+  
   if (
     (document.getElementById("User").value == User.username &&
       document.getElementById("Pass").value == DeCoder(User.password)) ||
@@ -483,9 +514,9 @@ function LogingIn() {
 }
 
 function LogingOut() {
-  // sessionStorage.setItem("Logined", "false;");
-  // sessionStorage.setItem("SavedUser", "");
-  // window.location.href = "index.html";
+  sessionStorage.setItem("Logined", "false;");
+  sessionStorage.setItem("SavedUser", "");
+  window.location.href = "index.html";
 }
 
 function PageChanger(e, saved) {
@@ -1011,7 +1042,7 @@ function PasswordChange() {
     document.getElementById("newPass").value &&
     document.getElementById("oldPass").value &&
     document.getElementById("oldPass").value ==
-      DeCoder(sessionStorage.getItem("SavedUser")).split(";")[1]
+      DeCoder(User.password)
   ) {
     var icon =
       '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="mb-1 bi bi-check2-circle" viewBox="0 0 16 16">' +
@@ -1073,3 +1104,7 @@ function DeCoder(Data) {
   }
   return "";
 }
+
+
+
+
