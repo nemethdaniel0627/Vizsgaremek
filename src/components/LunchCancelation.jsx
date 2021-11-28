711ab612878e625e41eb304517b7f5341df46230
nemethdaniel0627 <nem.dani0627@gmail.com>
2021-11-28 10:49:50 +0100

    **front-end ebedlemondas folytatása** Lemondás dátum alapján elkezdve

--- src/components/LunchCancelation.jsx
+++ src/components/LunchCancelation.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import DateSelector from "./DateSelector";
 import Menu from "./Menu";
 
 export default function LunchCancelation() {
+    const [isMenuChecked, setIsMenuChecked] = useState(false);
+
+    function checkChange() {
+        setIsMenuChecked(!isMenuChecked)
+    }
     return(
         <div>
             <div className="lunch-cncl--menu">
-                <input className="lunch-cncl--menu--input" defaultChecked={true} type="radio" name="lunchCancelation" id="etlapCancel" />
-                <input className="lunch-cncl--menu--input" type="radio" name="lunchCancelation" id="datumCancel" />
+                <input onChange={checkChange} className="lunch-cncl--menu--input" defaultChecked={isMenuChecked} type="radio" name="lunchCancelation" id="etlapCancel" />
+                <input onChange={checkChange} className="lunch-cncl--menu--input" defaultChecked={!isMenuChecked} type="radio" name="lunchCancelation" id="datumCancel" />
                 <label htmlFor="etlapCancel" id="etlapCancelItem" className="lunch-cncl--menu--item">Étlap alapú lemondás</label>
                 <label htmlFor="datumCancel" id="datumCancelItem" className="lunch-cncl--menu--item">Dátum alapú lemondás</label>
             </div>
-            <Menu header="Lemondás"/>
+            {isMenuChecked ? <Menu header="Lemondás"/> : <DateSelector />}
         </div>
     )
 }
