85ce28f8f4b322067ae481e8c3e6caa3cc3a679b
rozsnono <rozsnono@gmail.com>
2021-12-04 18:27:45 +0100

    **Ebédjegy** Fixes #40

--- src/components/DesktopTicket.jsx
+++ src/components/DesktopTicket.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Ticket from "./Ticket";
 import ticketImg from "../images/icon.png";
+import QRcode from "qrcode.react";
 
 export default function DesktopTicket() {
     return (
@@ -9,17 +10,18 @@ export default function DesktopTicket() {
                 <div className="row">
                     <div className="col-lg-4 col-sm-12 col-md-12">
                         <img src={ticketImg} alt="Icon" className="mx-auto d-none d-lg-block align-center" id="Ticket_img" />
-                        <div className="qrcode w-100 d-flex justify-content-center d-block d-lg-none" id="qrcode">
+                        <div class="qrcode w-100 d-flex justify-content-center d-block d-lg-none p-3" id="qrcode">
+                            <Ticket data = "google.com"  size = "256"/>
                         </div>
                     </div>
                     <div className="col-lg-8 col-sm-12 border-start border-dark d-none d-lg-block" id="Ticket_infos">
                         <h2 id="Ticket_name" className="mb-1 ms-1 fs-1">Vezetéknév Keresztnév Utónév</h2>
                         <h5 id="Ticket_class" className="ms-1">Osztály</h5>
                         <h1 id="Ticket_info" className="text-center mt-3 ms-1">
-                        <strong>Befizetve</strong>
+                            <strong>Befizetve</strong>
                         </h1>
                         <div className="date mt-4 ms-1">
-                        <p>Dátum: ----------</p>
+                            <p>Dátum: ----------</p>
                         </div>
                     </div>
                 </div>
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -3,28 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import axios from "axios";
 import { URL } from "../utils/constants";
-import QRCode from "qrcode.react";
+import QRcode from "qrcode.react";
 
-export default function Ticket()
+export default function Ticket(props)
 {
-    function QRcodeLoader(dataRow) {
-        let website = dataRow;
-        if (website) {
-
-            var qrcodeContainer = document.createElement('div');
-            qrcodeContainer.innerHTML = "";
-            new QRCode(qrcodeContainer, {
-            text: website,
-            title: "",
-            colorDark: "#000000",
-            colorLight: "#ffffff00",
-            correctLevel: QRCode.CorrectLevel.H,
-            });
-        } else {
-            console.log('Error');
-        }
-        return qrcodeContainer;
-    }
-
-    return QRcodeLoader('Valami.valami.valami,valami');
+    return (<QRcode value={props.data} bgColor="#ffffff00" width="20%" height='20%' size={props.size} />);
 }
