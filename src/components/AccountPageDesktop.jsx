c4732e705355a84c09c18d8872332c7be587b05c
rozsnono <rozsnono@gmail.com>
2022-01-27 08:28:37 +0100

    **Adatmodosítás** Fixes #110

--- src/components/AccountPageDesktop.jsx
+++ src/components/AccountPageDesktop.jsx
@@ -1,64 +1,148 @@
 import {
-    faLock,
-    faPencilAlt,
-    faSyncAlt,
-    faTimesCircle,
-    faUnlock,
-  } from "@fortawesome/free-solid-svg-icons";
-  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-  import React from "react";
-  import Activities from "./AccountPageActivities";
-  import { useState } from "react";
-  
-  export default function DataPage(user) {
-  
-    const [change, changing] = useState(false);
-  
-    function PassChange() {
-      changing(!change);
-    }
+  faLock,
+  faPencilAlt,
+  faSyncAlt,
+  faTimesCircle,
+  faUnlock,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import Activities from "./AccountPageActivities";
+import { useState } from "react";
 
-    console.log(user.user.user);
-  
-    return (
-      <div className="h3 m-5">
-        <div className="container datas">
-          <div className="row">
-            <div className="col-9 personal-datas">
-              <div className="important">
-                <div className="header">
-                  <h1>Személyes adatok</h1>
-                  <button className="btn modify-btn">
+export default function DataPage(user) {
+  const [change, changing] = useState(false);
+  const [aChange, aChanging] = useState(false);
+
+  function PassChange() {
+    changing(!change);
+  }
+
+  function AccChange() {
+    aChanging(!aChange);
+  }
+
+  console.log(user.user.user);
+
+  return (
+    <div className="h3 m-5">
+      <div className="container datas">
+        <div className="row">
+          <div className="col-9 personal-datas">
+            <div className="important">
+              <div className="header">
+                <h1>Személyes adatok</h1>
+                {aChange ? (
+                  <button
+                    className="btn modify-btn text-danger border-danger"
+                    onClick={AccChange}
+                  >
+                    {" "}
+                    Mégsem <FontAwesomeIcon icon={faTimesCircle} />
+                  </button>
+                ) : (
+                  <button className="btn modify-btn" onClick={AccChange}>
                     {" "}
                     Módosítás <FontAwesomeIcon icon={faPencilAlt} />
                   </button>
-                </div>
-  
-                <table className="personal-tables">
-                  <tbody>
-                    <tr>
-                      <td className="key">Vezetéknév</td>
-                      <td className="value">{user.user.user.vNev}</td>
-                      <td className="key">Osztály</td>
-                      <td className="value">{user.user.user.osztaly}</td>
-                    </tr>
-                    <tr>
-                      <td className="key">Keresztnév</td>
-                      <td className="value">{user.user.user.kNev}</td>
-                      <td className="key">Iskola OM azonosító</td>
-                      <td className="value">{user.user.user.iskolaOm}</td>
-                    </tr>
-                    <tr>
-                      <td className="key">Felhasználónév</td>
-                      <td className="value">{user.user.user.om}</td>
-                      <td className="key">E-mail cím</td>
-                      <td className="value">{user.user.user.email}</td>
-                    </tr>
-                  </tbody>
-                  
-                </table>
+                )}
               </div>
-              {/* <div className="bank-account">
+
+              <table className="personal-tables">
+                <tbody>
+                  <tr>
+                    <td className="key">Vezetéknév</td>
+
+                    {aChange ? (
+                      <td>
+                        <input
+                          className="form-input"
+                          value={user.user.user.vNev}
+                          disabled
+                        />
+                      </td>
+                    ) : (
+                      <td className="value">{user.user.user.vNev}</td>
+                    )}
+                    <td className="key">Osztály</td>
+                    {aChange ? (
+                      <td>
+                        <input
+                          className="form-input"
+                          value={user.user.user.osztaly}
+                          disabled
+                        />
+                      </td>
+                    ) : (
+                      <td className="value">{user.user.user.osztaly}</td>
+                    )}
+                  </tr>
+                  <tr>
+                    <td className="key">Keresztnév</td>
+                    {aChange ? (
+                      <td>
+                        <input
+                          className="form-input"
+                          value={user.user.user.kNev}
+                          disabled
+                        />
+                      </td>
+                    ) : (
+                      <td className="value">{user.user.user.kNev}</td>
+                    )}
+                    <td className="key">Iskola OM azonosító</td>
+                    {aChange ? (
+                      <td>
+                        <input
+                          className="form-input"
+                          value={user.user.user.iskolaOm}
+                          disabled
+                        />
+                      </td>
+                    ) : (
+                      <td className="value">{user.user.user.iskolaOm}</td>
+                    )}
+                  </tr>
+                  <tr>
+                    <td className="key">OM azonosító</td>
+                    {aChange ? (
+                      <td>
+                        <input
+                          className="form-input"
+                          value={user.user.user.om}
+                          disabled
+                        />
+                      </td>
+                    ) : (
+                      <td className="value">{user.user.user.om}</td>
+                    )}
+                    <td className="key">E-mail cím</td>
+                    {aChange ? (
+                      <td>
+                        <input
+                          className="form-input"
+                          value={user.user.user.email}
+                        />
+                      </td>
+                    ) : (
+                      <td className="value">{user.user.user.email}</td>
+                    )}
+                  </tr>
+                  {aChange ? (
+                    <tr>
+                      <td colSpan={4} className="text-center">
+                        <button className="btn passChange-btn">
+                          Módosítás
+                        </button>
+                      </td>
+                    </tr>
+                  ) : (
+                    <></>
+                  )}
+                </tbody>
+              </table>
+            </div>
+            {/* <div className="bank-account">
                 <div className="header">
                   <h1>Banki adatok</h1>
                   <button className="btn delete-btn">
@@ -86,21 +170,29 @@ import {
                   </tr>
                 </table>
               </div> */}
-              <div className="password-change">
-                <div className="header">
-                  <h1>Jelszó módosítás</h1>
-                  {change ?
-                    <button className="btn modify-btn text-danger border-danger" onClick={PassChange}>
-                      {" "}
-                      Mégsem <FontAwesomeIcon icon={faTimesCircle} />
-                    </button> :
-                    <button className="btn modify-btn" onClick={PassChange}>
-                      {" "}
-                      Módosítás <FontAwesomeIcon icon={faPencilAlt} />
-                    </button>}
-                </div>
-  
-                {!change ? <></> : <table className="personal-tables">
+            <div className="password-change">
+              <div className="header">
+                <h1>Jelszó módosítás</h1>
+                {change ? (
+                  <button
+                    className="btn modify-btn text-danger border-danger"
+                    onClick={PassChange}
+                  >
+                    {" "}
+                    Mégsem <FontAwesomeIcon icon={faTimesCircle} />
+                  </button>
+                ) : (
+                  <button className="btn modify-btn" onClick={PassChange}>
+                    {" "}
+                    Módosítás <FontAwesomeIcon icon={faPencilAlt} />
+                  </button>
+                )}
+              </div>
+
+              {!change ? (
+                <></>
+              ) : (
+                <table className="personal-tables">
                   <tr>
                     <td className="key">Régi jelszó</td>
                     <td className="value">
@@ -132,44 +224,43 @@ import {
                     </td>
                   </tr>
                   <tr>
-                    <td colSpan={4} className="text-center"><button className="btn passChange-btn">Módosítás</button></td>
+                    <td colSpan={4} className="text-center">
+                      <button className="btn passChange-btn">Módosítás</button>
+                    </td>
                   </tr>
-                </table>}
-              </div>
+                </table>
+              )}
             </div>
-            <div className="col-3 personal-activities">
-              <div className="activities desktop">
-                <div className="header">
-                  <h1>Tevékenység</h1>
-                  <button className="btn refresh-btn ">
-                    <FontAwesomeIcon icon={faSyncAlt} />
-                  </button>
-                </div>
-                <hr />
-                <Activities
-                  activity="Befizetett ebéd"
-                  date="2022.01.04"
-                  type="pay"
-                ></Activities>
-                <Activities
-                  activity="Lemondott nap(ok)"
-                  date="2021.12.20"
-                  type="cancel"
-                ></Activities>
-                <Activities
-                  activity="????????"
-                  date="2021.12.20"
-                ></Activities>
-                <Activities
-                  activity="Adatmódosítás"
-                  date="2022.01.24"
-                  type="modify"
-                ></Activities>
+          </div>
+          <div className="col-3 personal-activities">
+            <div className="activities desktop">
+              <div className="header">
+                <h1>Tevékenység</h1>
+                <button className="btn refresh-btn ">
+                  <FontAwesomeIcon icon={faSyncAlt} />
+                </button>
               </div>
+              <hr />
+              <Activities
+                activity="Befizetett ebéd"
+                date="2022.01.04"
+                type="pay"
+              ></Activities>
+              <Activities
+                activity="Lemondott nap(ok)"
+                date="2021.12.20"
+                type="cancel"
+              ></Activities>
+              <Activities activity="????????" date="2021.12.20"></Activities>
+              <Activities
+                activity="Adatmódosítás"
+                date="2022.01.24"
+                type="modify"
+              ></Activities>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
--- src/components/AccountPageMobile.jsx
+++ src/components/AccountPageMobile.jsx
@@ -17,6 +17,12 @@ export default function DataPage(props) {
     changing(!change);
   }
 
+  const [aChange, aChanging] = useState(false);
+
+  function AccChange() {
+    aChanging(!aChange);
+  }
+
   return (
     <div className="h3 mt-5">
       <div className="container datas">
@@ -25,40 +31,157 @@ export default function DataPage(props) {
             <div className="important">
               <div className="header">
                 <h1 className="mobile-h1">Személyes adatok</h1>
-                {/* <button className="btn modify-btn">
-                                    {" "}
-                                    Módosítás <FontAwesomeIcon icon={faPencilAlt} />
-                                </button> */}
+                {aChange ? (
+                  <button
+                    className="btn modify-btn modify-btn-mobile text-danger border-danger"
+                    onClick={AccChange}
+                  >
+                    {" "}
+                    Mégsem <FontAwesomeIcon icon={faTimesCircle} />
+                  </button>
+                ) : (
+                  <button
+                    className="btn modify-btn modify-btn-mobile"
+                    onClick={AccChange}
+                  >
+                    {" "}
+                    Módosítás <FontAwesomeIcon icon={faPencilAlt} />
+                  </button>
+                )}
               </div>
 
               <table className="personal-tables fs-6">
-                <tr>
-                  <td className="key">Vezetéknév</td>
-                  <td className="value">{props.user.user.vNev}</td>
-                </tr>
+                <tbody>
+                  <tr>
+                    <td className="key">Vezetéknév</td>
+                    {aChange ? (
+                      <td>
+                        <input
+                          className="form-input"
+                          value={props.user.user.vNev}
+                          disabled
+                        />
+                      </td>
+                    ) : (
+                      <td className="value">{props.user.user.vNev}</td>
+                    )}
+                  </tr>
 
-                <tr>
-                  <td className="key">Osztály</td>
-                  <td className="value">{props.user.user.osztaly}</td>
-                </tr>
-                <tr>
-                  <td className="key">Keresztnév</td>
-                  <td className="value">{props.user.user.kNev}</td>
-                </tr>
+                  <tr>
+                    <td className="key">Keresztnév</td>
+                    {aChange ? (
+                      <td>
+                        <input
+                          className="form-input"
+                          value={props.user.user.kNev}
+                          disabled
+                        />
+                      </td>
+                    ) : (
+                      <td className="value">{props.user.user.kNev}</td>
+                    )}
+                  </tr>
 
-                <tr>
-                  <td className="key">Iskola OM azonosító</td>
-                  <td className="value">{props.user.user.iskolaOm}</td>
-                </tr>
-                <tr>
-                  <td className="key">Felhasználónév</td>
-                  <td className="value">{props.user.user.om}</td>
-                </tr>
-                <tr>
-                  <td className="key">E-mail cím</td>
-                  <td className="value">{props.user.user.email}</td>
-                </tr>
+                  <tr>
+                    <td className="key">Osztály</td>
+                    {aChange ? (
+                      <td>
+                        <input
+                          className="form-input"
+                          value={props.user.user.osztaly}
+                          disabled
+                        />
+                      </td>
+                    ) : (
+                      <td className="value">{props.user.user.osztaly}</td>
+                    )}
+                  </tr>
+
+                  <tr>
+                    <td className="key">Iskola OM azonosító</td>
+                    {aChange ? (
+                      <td>
+                        <input
+                          className="form-input"
+                          value={props.user.user.iskolaOm}
+                          disabled
+                        />
+                      </td>
+                    ) : (
+                      <td className="value">{props.user.user.iskolaOm}</td>
+                    )}
+                  </tr>
+                  <tr>
+                    <td className="key">OM azonosító</td>
+                    {aChange ? (
+                      <td>
+                        <input
+                          className="form-input"
+                          value={props.user.user.om}
+                          disabled
+                        />
+                      </td>
+                    ) : (
+                      <td className="value">{props.user.user.om}</td>
+                    )}
+                  </tr>
+                  <tr>
+                    <td className="key">E-mail cím</td>
+                    {aChange ? (
+                      <td>
+                        <input
+                          className="form-input"
+                          value={props.user.user.email}
+                        />
+                      </td>
+                    ) : (
+                      <td className="value">{props.user.user.email}</td>
+                    )}
+                  </tr>
+                  {aChange ? (
+                    <tr>
+                      <td colSpan={4} className="text-center">
+                        <button className="btn passChange-btn">
+                          Módosítás
+                        </button>
+                      </td>
+                    </tr>
+                  ) : (
+                    <></>
+                  )}
+                </tbody>
               </table>
+            </div>
+
+            <div className="col-12 personal-activities">
+              <div className="activities">
+                <div className="header">
+                  <h1 className="mobile-h1">Tevékenység</h1>
+                  <button className="btn refresh-btn mobile-btn">
+                    <FontAwesomeIcon icon={faSyncAlt} />
+                  </button>
+                </div>
+                <hr />
+                <Activities
+                  activity="Befizetett ebéd"
+                  date="2022.01.04"
+                  type="pay"
+                ></Activities>
+                <Activities
+                  activity="Lemondott nap(ok)"
+                  date="2021.12.20"
+                  type="cancel"
+                ></Activities>
+                <Activities
+                  activity="?????"
+                  date="2021.12.20"
+                ></Activities>
+                <Activities
+                  activity="Adatmódosítás"
+                  date="2022.01.24"
+                  type="modify"
+                ></Activities>
+              </div>
             </div>
 
             <div className="password-change">
@@ -79,6 +202,7 @@ export default function DataPage(props) {
                 <></>
               ) : (
                 <table className="personal-tables">
+                  <tbody>
                   <tr>
                     <td className="key">Régi jelszó</td>
                   </tr>
@@ -120,38 +244,9 @@ export default function DataPage(props) {
                       <button className="btn passChange-btn">Módosítás</button>
                     </td>
                   </tr>
+                  </tbody>
                 </table>
               )}
-            </div>
-          </div>
-          <div className="col-12 personal-activities">
-            <div className="activities">
-              <div className="header">
-                <h1 className="mobile-h1">Tevékenység</h1>
-                <button className="btn refresh-btn mobile-btn">
-                  <FontAwesomeIcon icon={faSyncAlt} />
-                </button>
-              </div>
-              <hr />
-              <Activities
-                activity="Befizetett ebéd"
-                date="2022.01.04"
-                type="pay"
-              ></Activities>
-              <Activities
-                activity="Lemondott nap(ok)"
-                date="2021.12.20"
-                type="cancel"
-              ></Activities>
-              <Activities
-                activity="Lemondott nap(ok)"
-                date="2021.12.20"
-              ></Activities>
-              <Activities
-                activity="Adatmódosítás"
-                date="2022.01.24"
-                type="modify"
-              ></Activities>
             </div>
           </div>
         </div>
