92223e6bf7814f8d0f4e0da87546932aabf790f1
rozsnono <rozsnono@gmail.com>
2022-01-30 09:59:05 +0100

    Kapcsolat

--- src/components/ReportPage.jsx
+++ src/components/ReportPage.jsx
@@ -7,10 +7,15 @@ import foodE_icon from "../images/icon.png";
 export default function ReportPage() {
 
   const [error, setError] = useState(false);
+  const [email, setEmail] = useState(false);
 
-  function ErrorSendRequest(){
+  function ErrorSendRequest() {
     setError(!error);
 
+  }
+
+  function EmailModal() {
+    setEmail(!email);
   }
 
   return (
@@ -18,39 +23,39 @@ export default function ReportPage() {
     <div className="Report">
       <hr className="header--hr" />
       <div className="header">
-        <div className="row mx-auto justify-content-center d-flex">
-          <div className="col-12 col-lg-5"><h1 className="">Kapcsolat felvétel</h1></div>
-          <div className="col-0 col-lg-1 d-none d-lg-block"><img src={foodE_icon} alt="icon" className="icon"/></div>
+        <div className="row text-center">
+          <h1 className="">Kapcsolat felvétel</h1>
         </div>
-        
-        
+
+
       </div>
       <div className="report-body">
         <div className="container">
           <div className="row justify-content-center d-flex">
             <span className="--space"></span>
-            <div className="col-12 col-lg-4"><ReportPageCard title="Hiba jelentés" type="error" button="true" ErrorSendRequest={ErrorSendRequest}/></div>
+            <div className="col-12 col-lg-4"><ReportPageCard title="Hiba jelentés" type="error" button="true" ErrorSendRequest={ErrorSendRequest} /></div>
             <div className="col-0 col-lg-2"></div>
             <span className="--space d-flex d-lg-none"></span>
             <span className="--space d-flex d-lg-none"></span>
-            <div className="col-0 col-lg-4"><ReportPageCard title="Facebook" type="facebook" button="true" ErrorSendRequest={ErrorSendRequest}/></div>
+            <div className="col-0 col-lg-4"><ReportPageCard title="Facebook" type="facebook" button="true" /></div>
 
             <span className="--space"></span>
-            <div className="col-12 col-lg-4"><ReportPageCard title="foodE@business.gmail.com" fontSize="small" type="text" header="E-mail:"/></div>
+            <div className="col-12 col-lg-4"><ReportPageCard title="E-mail küldés" type="email" button="true" EmailModal={EmailModal} /></div>
+
             <div className="col-12 col-lg-2"></div>
             <span className="--space d-flex d-lg-none"></span>
-            <div className="col-12 col-lg-4"><ReportPageCard title="+36 40 123 7555" fontSize="small" type="text" header="Telefon:"/></div>
+            <div className="col-12 col-lg-4"><ReportPageCard title="+36 40 123 7555" fontSize="small" type="text" header="Telefon:" /></div>
 
             <span className="--space"></span>
-            <div className="col-12 col-lg-4"><ReportPageCard title="5231 Fegyvernek, Fő út 8" fontSize="small" type="text" header="Cím:"/></div>
+            <div className="col-12 col-lg-4"><ReportPageCard title="foodE@business.gmail.com" fontSize="small" type="text" header="E-mail:" /></div>
             <div className="col-12 col-lg-2"></div>
             <span className="--space d-flex d-lg-none"></span>
-            <div className="col-12 col-lg-4"><ReportPageCard title="?????" fontSize="small" type="text" header="?????:"/></div>
+            <div className="col-12 col-lg-4"><ReportPageCard title="5231 Fegyvernek, Fő út 8" fontSize="small" type="text" header="Cím:" /></div>
 
           </div>
         </div>
-        <ReportPageModal show={error} ModalClose = {ErrorSendRequest}/>
-        
+        <ReportPageModal show={error || email} type={error ? "error" : email ? "email" : ""} ModalClose={error ? ErrorSendRequest : email ? EmailModal : <></>} />
+
       </div>
     </div>
   );
--- src/components/ReportPageModal.jsx
+++ src/components/ReportPageModal.jsx
@@ -1,41 +1,63 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExclamationTriangle, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import { faExclamationTriangle, faPaperPlane, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 
-export default function AdminDatabaseModal(props) {
-    const [error, setError] = useState(props.show);
+export default function ReportPageModal(props) {
 
     function ModalClose() {
-        setError(!error);
         props.ModalClose();
     }
 
 
+    function ErrorModal() {
+        return (
+            <div className="fs-4 admin-modal">
+                <div className="input-group mb-3">
+                    <label htmlFor="where_error" className="mb-2">Hol a hiba?</label>
+                    <input type="text" id="where_error" className="w-100 form-control" />
+                </div>
+                <div className="input-group mb-3">
+                    <label htmlFor="error" className="mb-2">Hiba rövid leírása:</label>
+                    <textarea className="w-100 form-control" name="" id="error" cols="30" rows="10"></textarea>
+                </div>
+
+            </div>
+        );
+    }
+
+    function EmailModal() {
+        return (
+            <div className="fs-4 admin-modal">
+                <div className="input-group mb-3">
+                    <label htmlFor="where_error" className="mb-2">Tárgy:</label>
+                    <input type="text" id="where_error" className="w-100 form-control" />
+                </div>
+                <div className="input-group mb-3">
+                    <label htmlFor="email" className="mb-2">Tartalom:</label>
+                    <textarea className="w-100 form-control" name="" id="email" cols="30" rows="10"></textarea>
+                </div>
+
+            </div>
+        );
+    }
+
 
     return (
         <div >
-            <Modal show={props.show}>
+            <Modal show={props.show} className="Report-Modal">
                 <Modal.Header closeButton>
-                    <Modal.Title><span className="fw-bold fs-2">Hiba jelentés</span></Modal.Title>
+                    <Modal.Title>{props.type === "error" ? <span className="fw-bold fs-2">Hiba jelentés</span> : props.type === "email" ? <span className="fw-bold fs-2">E-mail küldés</span> : <></>}</Modal.Title>
                 </Modal.Header>
-                <Modal.Body className="Report-Modal">
-                    <div className="fs-4 admin-modal">
-                        <div className="input-group mb-3">
-                            <label htmlFor="where_error" className="mb-2">Hol a hiba?</label>
-                            <input type="text" id="where_error" className="w-100 form-control" />
-                        </div>
-                        <div className="input-group mb-3">
-                            <label htmlFor="error" className="mb-2">Hiba rövid leírása:</label>
-                            <textarea className="w-100 form-control" name="" id="error" cols="30" rows="10"></textarea>
-                        </div>
-
-                    </div>
+                <Modal.Body className="Report-Modal--body">
+                    {props.type === "error" ? <ErrorModal/> : props.type === "email" ? <EmailModal/> : <></>}
                 </Modal.Body>
-                <Modal.Footer className="Report-Modal">
-                    <button type="button" className="btn btn-send fs-4">
+                <Modal.Footer className="Report-Modal--body">
+                    {props.type === "error" ? <button type="button" className="btn btn-send fs-4">
                         <FontAwesomeIcon icon={faExclamationTriangle} /> <span> Jelentés</span>
-                    </button>
+                    </button> : props.type === "email" ? <button type="button" className="btn btn-send fs-4">
+                        <FontAwesomeIcon icon={faPaperPlane} /> <span> Küldés</span>
+                    </button> : <></>}
                     <button
                         type="button"
                         className="btn btn-cancel fs-4"
