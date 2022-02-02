946aeacf7b91c096c922b6ce312029014d1eb196
rozsnono <rozsnono@gmail.com>
2022-02-02 20:17:22 +0100

    Fixed

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -5,22 +5,27 @@ import Mobile from "../layouts/AccountPageMobile";
 
 export default function Page(user) {
 
-  const [isMobile, setMobile] = useState(false);
+  const [isLittleScreen, setScreen] = useState(false);
+
+  let isMobile = false;
 
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    setMobile(true);
+    isMobile = true;
   }
 
-  window.addEventListener('resize', Resizing)
+  if (isMobile === false) {
+    window.addEventListener('resize', Resizing)
+  }
 
-  function Resizing(){
-    setMobile(window.matchMedia("(max-width: 768px)").matches);
+  function Resizing() {
+    setScreen(window.matchMedia("(max-width: 768px)").matches);
   }
 
   return (
     <div>
-      {!isMobile ? <Desktop user={user} /> : <Mobile user={user} />}
-
+      {isMobile ? <Mobile user={user} /> : 
+        !isLittleScreen ? <Desktop user={user} /> : <Mobile user={user} />
+      }
     </div>
   );
 }
