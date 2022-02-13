c5ff80258b03000510f00ceb6de12ab9da19a393
Phrodo02 <martinbalazs0415@gmail.com>
2022-02-13 14:55:10 +0100

    **QR kód adatellenőrzés** Beolvas kérés

--- server/server.js
+++ server/server.js
@@ -166,11 +166,18 @@ app.post("/cancel", async (req, res) => {
 app.post("/test", async (req, res) => {
   // const create = await test.generate('users2.txt', 82);
   // res.send(create);
-  // const mealsCount = await order.ordersCountByDate('2022-01-31');
-  // res.send(mealsCount);
   const testOrders = await test.orders('2022-02-04', 15);
   res.send(testOrders);
+})
 
+app.post("/beolvas", auth.tokenAutheticate, async (req, res) => {
+  const omAzon = req.body.omAzon;
+  const users = await user.getAll(false);
+  let userData;
+  users.forEach(user => {
+    if (user.omAzon === omAzon) userData = user;
+  });
+  res.send(userData.befizetve);
 })
 
 app.get("/", (req, res) => {
