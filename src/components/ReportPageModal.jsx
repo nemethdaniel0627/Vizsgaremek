a14387aed04bf5acffbc1ec7cf71d4e81fe4e924
rozsnono <rozsnono@gmail.com>
2022-03-16 12:19:49 +0100

    **Hiba jelentés fix** Fixes #171

--- src/components/ReportPageModal.jsx
+++ src/components/ReportPageModal.jsx
@@ -32,7 +32,7 @@ export default function ReportPageModal(props) {
 
     function Checking() {
         setError(false);
-        return document.getElementById('message_where').value !== "" && document.getElementById('message_what').value !== "" ? true : false;
+        return document.getElementById('message_where').value !== "" && document.getElementById('message_what').value !== "" && check ? true : false;
     }
 
     function sendEmail(e) {
@@ -47,6 +47,7 @@ export default function ReportPageModal(props) {
                         omAzon: user.omAzon,
                         where: document.getElementById('message_where').value,
                         what: document.getElementById('message_what').value,
+                        fromEmail: user.email,
                         type: "report"
                     },
                     {
@@ -141,7 +142,7 @@ export default function ReportPageModal(props) {
                     {sending ? <EmailSending /> : sended ? <EmailSendedBody /> : props.type === "error" ? <ErrorModal /> : props.type === "email" ? <EmailModal /> : <></>}
                 </Modal.Body>
                 <Modal.Footer className="Report-Modal--body">
-                    {sended ? <></> : props.type === "error" ? <button type="submit" className="btn btn-send fs-4" onClick={sendEmail} disabled={!check}>
+                    {sended ? <></> : props.type === "error" ? <button type="submit" className="btn btn-send fs-4" onClick={sendEmail} >
                         <FontAwesomeIcon icon={faExclamationTriangle} /> <span> Jelentés</span>
                     </button> : props.type === "email" ? <button type="submit" className="btn btn-send fs-4" disabled={sending}>
                         <FontAwesomeIcon icon={faPaperPlane} /> <span> Küldés</span>
