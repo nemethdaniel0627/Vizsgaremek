2f836d4084cd44fce3c8b80f410f23c68d8e7638
rozsnono <rozsnono@gmail.com>
2022-03-17 08:02:09 +0100

    Tooltip

--- src/components/AccountPageActivities.jsx
+++ src/components/AccountPageActivities.jsx
@@ -1,29 +1,43 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckCircle, faClock, faTimesCircle, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheckCircle,
+  faClock,
+  faTimesCircle,
+  faUserCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
-export default function Activities(props)
-{
-    return(
-        <div className="act">
-            <div className="container">
-                <div className="row">
-                    <div className={"col-2 "  + props.type}>
-                        {props.type==="pay" ? 
-                        <FontAwesomeIcon icon={faCheckCircle} className="fs-1"/> : 
-                        props.type==="cancel" ? 
-                        <FontAwesomeIcon icon={faTimesCircle} className="fs-1"/> :
-                        props.type==="modify" ? 
-                        <FontAwesomeIcon icon={faUserCircle} className="fs-1"/> : 
-                        <FontAwesomeIcon icon={faClock} className="fs-1"/>
-                        }
-                    </div>
-                    <div className="col-10">
-                        <h3 className="act-name">{props.activity}</h3>
-                        <h5 className="act-date">{props.date}</h5>
-                    </div>
-                </div>
-            </div>
+export default function Activities(props) {
+  return (
+    <div className="act">
+      <div className="container">
+        <div className="row">
+          <div className={"col-2 " + props.type}>
+            {props.type === "pay" ? (
+              <FontAwesomeIcon icon={faCheckCircle} className="fs-1" />
+            ) : props.type === "cancel" ? (
+              <FontAwesomeIcon icon={faTimesCircle} className="fs-1" />
+            ) : props.type === "modify" ? (
+              <FontAwesomeIcon icon={faUserCircle} className="fs-1" />
+            ) : (
+              <FontAwesomeIcon icon={faClock} className="fs-1" />
+            )}
+          </div>
+          <div className="col-10">
+            <h3 className="act-name">{props.activity}</h3>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-toggle="tooltip"
+              data-placement="bottom"
+              title={props.dates}
+            >
+              Tooltip on bottom
+            </button>
+            <h5 className="act-date">{props.date}</h5>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
