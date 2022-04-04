d44205a11ec869ec3b2b5eb5da6bffe3e102bbeb
nemethdaniel0627 <nem.dani0627@gmail.com>
2022-04-04 09:27:13 +0200

    SQL connection for heroku Fixes #190

--- server/modules/sqlQueries.js
+++ server/modules/sqlQueries.js
@@ -4,9 +4,10 @@ class sqlQueries {
 
     async CreateConnection(array = true) {
         this._connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            database: 'foode',
+            host: 'eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+            user: 'gv0mvqhvp3pzbc4o',
+            password: 'dbs0wqnd0vvxkjk2',
+            database: 's2v7anrhwduvuhqp',
             rowsAsArray: array
         });
     }
